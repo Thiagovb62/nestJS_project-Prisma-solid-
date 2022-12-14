d07818b525635b4a53f2280ea065c98105e71f2b
Thiago Viana Bezerra <Thiagovb62@gmail.com>
2022-12-14 16:00:15 -0300

    inicializaçao do projeto nestjs

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { PostmarkMailService } from './mail/postmark-mail.service';
+import { SMTPMailService } from './mail/smtp-mail-service';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { MailService } from './mail/mail.service';
+
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [{
+  provide: MailService,
+  useClass: PostmarkMailService,
+  }],
+})
+export class AppModule {}
